6311dadb86280623f253bc5885c83e99900636fc
Anastasiya Kaskevich <106557182+naszh@users.noreply.github.com>
2022-10-24 09:15:46 +0300

    comment cache in cache.js

--- cache.js
+++ cache.js
@@ -1,44 +1,44 @@
-const cacheName = 'v1';
+// const cacheName = 'v1';
 
-const cacheAssets = [
-  'index.html',
-  'script.js',
-  'styles.css',
-]
+// const cacheAssets = [
+//   'index.html',
+//   'script.js',
+//   'styles.css',
+// ]
 
-self.addEventListener('install', e => {
-  // console.log('Service Worker: Installed');
+// self.addEventListener('install', e => {
+//   // console.log('Service Worker: Installed');
 
-  e.waitUntil(
-    caches
-      .open(cacheName)
-      .then(cache => {
-        // console.log('Service Worker: Caching Files');
-        cache.addAll(cacheAssets);
-      })
-      .then(() => self.skipWaiting())
-  );
-});
+//   e.waitUntil(
+//     caches
+//       .open(cacheName)
+//       .then(cache => {
+//         // console.log('Service Worker: Caching Files');
+//         cache.addAll(cacheAssets);
+//       })
+//       .then(() => self.skipWaiting())
+//   );
+// });
 
-self.addEventListener('activate', e => {
-  // console.log('Service Worker: Activated');
-  e.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== cacheName) {
-            // console.log('Service Worker: Clearing Old Cache');
-            return caches.delete(cache);
-          }
-        })
-      )
-    })
-  )
-});
+// self.addEventListener('activate', e => {
+//   // console.log('Service Worker: Activated');
+//   e.waitUntil(
+//     caches.keys().then(cacheNames => {
+//       return Promise.all(
+//         cacheNames.map(cache => {
+//           if (cache !== cacheName) {
+//             // console.log('Service Worker: Clearing Old Cache');
+//             return caches.delete(cache);
+//           }
+//         })
+//       )
+//     })
+//   )
+// });
 
-self.addEventListener('fetch', e => {
-  // console.log('Service Worker: Fetching');
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  )
-})
+// self.addEventListener('fetch', e => {
+//   // console.log('Service Worker: Fetching');
+//   e.respondWith(
+//     fetch(e.request).catch(() => caches.match(e.request))
+//   )
+// })
